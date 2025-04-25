0de6f8a09193902654bb8c98c078d1b3de1b1afa
TheRavenStark <113565611+TheRavenStark@users.noreply.github.com>
2025-04-25 12:31:43 -0600

    Update Proyecto.java

--- lastProjectBD/src/java/Modelo/Proyecto.java
+++ lastProjectBD/src/java/Modelo/Proyecto.java
@@ -1,34 +1,31 @@
+
 package Modelo;
 
-import java.sql.Date;
-
+import java.util.Date;
+/**
+ *
+ * @author adria
+ */
 public class Proyecto {
-    private int proyectoId; // PROYECTO_ID
-    private String nombreProyecto; // NOMBRE_PROYECTO
-    private String descripcion; // DESCRIPCION
-    private Date fechaCreacion; // FECHA_CREACION
-    private int usuarioId; // USUARIO_ID
-    private String createdBy; // CREATED_BY
-    private Date creationDate; // CREATION_DATE
-    private String lastUpdateBy; // LAST_UPDATE_BY
-    private Date lastUpdateDate; // LAST_UPDATE_DATE
-    private String accion; // ACCION
-
-    // Getters y Setters
-    public int getProyectoId() {
-        return proyectoId;
+    private Integer id_proyecto;
+    private String nombre;
+    private String descripcion;
+    
+    
+    public Integer getId_proyecto() {
+        return id_proyecto;
     }
 
-    public void setProyectoId(int proyectoId) {
-        this.proyectoId = proyectoId;
+    public void setId_proyecto(Integer id_proyecto) {
+        this.id_proyecto = id_proyecto;
     }
 
-    public String getNombreProyecto() {
-        return nombreProyecto;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setNombreProyecto(String nombreProyecto) {
-        this.nombreProyecto = nombreProyecto;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getDescripcion() {
@@ -38,60 +35,6 @@ public class Proyecto {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
-    public Date getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(Date fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public int getUsuarioId() {
-        return usuarioId;
-    }
-
-    public void setUsuarioId(int usuarioId) {
-        this.usuarioId = usuarioId;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getLastUpdateBy() {
-        return lastUpdateBy;
-    }
-
-    public void setLastUpdateBy(String lastUpdateBy) {
-        this.lastUpdateBy = lastUpdateBy;
-    }
-
-    public Date getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(Date lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
-
-    public String getAccion() {
-        return accion;
-    }
-
-    public void setAccion(String accion) {
-        this.accion = accion;
-    }
+    
 }
+
